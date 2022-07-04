3151f133428d921374594284e7218f1fa552d0fc
geonya <geony8410@gmail.com>
2022-07-04 23:04:26 +0900

    CSS Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,229 +6,238 @@ import { routes } from '../sharedData';
 import { useSeeUser } from './hooks/useSeeUser';
 
 const Container = styled.div`
-	max-width: ${(props) => props.theme.maxWidth};
-	width: 100%;
-	height: 100vh;
-	margin: 0 auto;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	flex-direction: column;
-	position: relative;
-	box-shadow: ${(props) => props.theme.boxShadow};
+  max-width: ${(props) => props.theme.maxWidth};
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  box-shadow: ${(props) => props.theme.boxShadow};
+  border-radius: 50px;
+  overflow: hidden;
 `;
 
 const Header = styled.div`
-	width: 100%;
-	padding: 10px 40px;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	box-shadow: ${(props) => props.theme.boxShadow};
+  width: 100%;
+  padding: 25px 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
 `;
 const Content = styled.div`
-	overflow-y: scroll;
-	width: 100%;
-	height: 100%;
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	flex-direction: column;
+  overflow-y: scroll;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
 `;
 
-const BackBtn = styled.button`
-	position: absolute;
-	top: 8px;
-	left: 5px;
-	svg {
-		width: 30px;
-		height: 30px;
-	}
+const HeaderBtn = styled.button`
+  svg {
+    width: 30px;
+    height: 30px;
+  }
 `;
 const Footer = styled.div`
-	width: 100%;
-	padding: 10px 10px;
-	display: flex;
-	align-items: center;
-	justify-content: space-evenly;
-	box-shadow: ${(props) => props.theme.boxShadow};
+  width: 100%;
+  padding: 30px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
 `;
 
 const IconBtn = styled.button`
-	width: 25px;
-	height: 25px;
-	svg {
-		color: ${(props) => props.theme.fontColor};
-	}
+  width: 25px;
+  height: 25px;
+  svg {
+    color: ${(props) => props.theme.fontColor};
+  }
 `;
-const HeaderTitle = styled.span`
-	font-size: 20px;
-	font-weight: 600;
-`;
+
 const HeaderBtnContainer = styled.div`
-	display: flex;
+  display: flex;
 `;
 const HeartBtn = styled(IconBtn)`
-	margin-right: 10px;
-	svg {
-		color: ${(props) => props.theme.red};
-	}
+  margin-right: 20px;
+  svg {
+    color: ${(props) => props.theme.red};
+  }
 `;
 const DarkModeBtnContainer = styled.div`
-	position: relative;
-	width: 42px;
-	height: 26px;
+  position: relative;
+  width: 42px;
+  height: 26px;
 `;
 const DarkModeBtnLabel = styled.label`
-	position: absolute;
-	width: 42px;
-	height: 26px;
-	border-radius: 15px;
-	background: ${(props) => props.theme.fontColor};
-	cursor: pointer;
-	&::after {
-		content: '';
-		display: block;
-		border-radius: 50%;
-		width: 18px;
-		height: 18px;
-		margin: 3.5px;
-		background: ${(props) => props.theme.bgColor};
-		transition: 0.2s;
-	}
+  position: absolute;
+  width: 42px;
+  height: 26px;
+  border-radius: 15px;
+  background: ${(props) => props.theme.fontColor};
+  cursor: pointer;
+  &::after {
+    content: '';
+    display: block;
+    border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    margin: 3.5px;
+    background: ${(props) => props.theme.bgColor};
+    transition: 0.2s;
+  }
 `;
 const DarkModeBtn = styled.input`
-	opacity: 0;
-	z-index: 1;
-	border-radius: 50%;
-	width: 42px;
-	height: 26px;
-	&:checked + ${DarkModeBtnLabel} {
-		background: ${(props) => props.theme.fontColor};
-		&::after {
-			content: '';
-			display: block;
-			border-radius: 50%;
-			width: 18px;
-			height: 18px;
-			margin-left: 21px;
-			transition: 0.2s;
-		}
-	}
+  opacity: 0;
+  z-index: 1;
+  border-radius: 50%;
+  width: 42px;
+  height: 26px;
+  &:checked + ${DarkModeBtnLabel} {
+    background: ${(props) => props.theme.fontColor};
+    &::after {
+      content: '';
+      display: block;
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+    }
+  }
 `;
 interface ILayout {
-	children: React.ReactNode;
-	hasHeader?: boolean;
-	hasFooter?: boolean;
+  children: React.ReactNode;
+  hasHeader?: boolean;
+  hasFooter?: boolean;
 }
 const Layout = ({ children, hasHeader = true, hasFooter = true }: ILayout) => {
-	const navigate = useNavigate();
-	const darkMode = useReactiveVar(darkModeVar);
-	const { data } = useSeeUser();
-	return (
-		<Container>
-			{hasHeader && (
-				<Header>
-					<BackBtn onClick={() => navigate('..', { replace: true })}>
-						<svg
-							fill='none'
-							stroke='currentColor'
-							viewBox='0 0 24 24'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth='2'
-								d='M15 19l-7-7 7-7'
-							/>
-						</svg>
-					</BackBtn>
-					<HeaderTitle>Nomad Coffee</HeaderTitle>
-					<HeaderBtnContainer>
-						<HeartBtn onClick={() => navigate(routes.notification)}>
-							<svg
-								fill='currentColor'
-								stroke='currentColor'
-								viewBox='0 0 24 24'
-								xmlns='http://www.w3.org/2000/svg'
-							>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									strokeWidth='2'
-									d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
-								></path>
-							</svg>
-						</HeartBtn>
-						<DarkModeBtnContainer>
-							<DarkModeBtn
-								id='checkbox'
-								type='checkbox'
-								onChange={() => toggleDarkMode(darkMode)}
-							/>
-							<DarkModeBtnLabel htmlFor='checkbox' />
-						</DarkModeBtnContainer>
-					</HeaderBtnContainer>
-				</Header>
-			)}
-			<Content>{children}</Content>
-			{hasFooter && (
-				<Footer>
-					<IconBtn onClick={() => navigate(routes.home)}>
-						{/* home */}
-						<svg
-							fill='none'
-							stroke='currentColor'
-							viewBox='0 0 24 24'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth='2'
-								d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
-							></path>
-						</svg>
-					</IconBtn>
-					<IconBtn onClick={() => navigate(routes.search)}>
-						{/* serach */}
-						<svg
-							fill='none'
-							stroke='currentColor'
-							viewBox='0 0 24 24'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth='2'
-								d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-							></path>
-						</svg>
-					</IconBtn>
-					<IconBtn
-						onClick={() => navigate(`/users/${data?.seeMyProfile.username}`)}
-					>
-						{/* user */}
-						<svg
-							fill='none'
-							stroke='currentColor'
-							viewBox='0 0 24 24'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth='2'
-								d='M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'
-							></path>
-						</svg>
-					</IconBtn>
-				</Footer>
-			)}
-		</Container>
-	);
+  const navigate = useNavigate();
+  const darkMode = useReactiveVar(darkModeVar);
+  const { data } = useSeeUser();
+  return (
+    <Container>
+      {hasHeader && (
+        <Header>
+          <HeaderBtn onClick={() => navigate('..', { replace: true })}>
+            <svg
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='2'
+                d='M4 6h16M4 12h8m-8 6h16'
+              ></path>
+            </svg>
+          </HeaderBtn>
+          {/* <HeaderBtn onClick={() => navigate('..', { replace: true })}>
+            <svg
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M15 19l-7-7 7-7'
+              />
+            </svg>
+          </HeaderBtn> */}
+
+          <HeaderBtnContainer>
+            <HeartBtn onClick={() => navigate(routes.notification)}>
+              <svg
+                fill='currentColor'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
+                ></path>
+              </svg>
+            </HeartBtn>
+            <DarkModeBtnContainer>
+              <DarkModeBtn
+                id='checkbox'
+                type='checkbox'
+                onChange={() => toggleDarkMode(darkMode)}
+              />
+              <DarkModeBtnLabel htmlFor='checkbox' />
+            </DarkModeBtnContainer>
+          </HeaderBtnContainer>
+        </Header>
+      )}
+      <Content>{children}</Content>
+      {hasFooter && (
+        <Footer>
+          <IconBtn onClick={() => navigate(routes.home)}>
+            {/* home */}
+            <svg
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
+              ></path>
+            </svg>
+          </IconBtn>
+          <IconBtn onClick={() => navigate(routes.search)}>
+            {/* serach */}
+            <svg
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+              ></path>
+            </svg>
+          </IconBtn>
+          <IconBtn
+            onClick={() => navigate(`/users/${data?.seeMyProfile.username}`)}
+          >
+            {/* user */}
+            <svg
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'
+              ></path>
+            </svg>
+          </IconBtn>
+        </Footer>
+      )}
+    </Container>
+  );
 };
 
 export default Layout;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,40 +5,138 @@ import { useSeeCafesQuery } from '../generated/graphql';
 import { routes } from '../sharedData';
 import CafeBox from '../components/CafeBox';
 
-const AddBtn = styled.button`
-	background-color: ${(props) => props.theme.pointColor};
-	border-radius: 50%;
-	position: absolute;
-	right: 10px;
-	bottom: 60px;
-	width: 50px;
-	height: 50px;
-	box-shadow: ${(props) => props.theme.boxShadow};
-	svg {
-		color: white;
-	}
-`;
 export default function Home() {
-	const navigation = useNavigate();
-	const { data, loading } = useSeeCafesQuery({ variables: { offset: 0 } });
-	return (
-		<Layout>
-			{loading
-				? 'loading...'
-				: data?.seeCafes?.map((cafe, i) => <CafeBox {...cafe} key={i} />)}
-			<AddBtn onClick={() => navigation(routes.add)}>
-				<svg
-					fill='currentColor'
-					viewBox='0 0 20 20'
-					xmlns='http://www.w3.org/2000/svg'
-				>
-					<path
-						fillRule='evenodd'
-						d='M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z'
-						clipRule='evenodd'
-					/>
-				</svg>
-			</AddBtn>
-		</Layout>
-	);
+  const navigation = useNavigate();
+  const { data, loading } = useSeeCafesQuery({ variables: { offset: 0 } });
+  return (
+    <Layout>
+      <Top>
+        <TopTitle>
+          <TopTitleSpan>
+            <TopTitleAccent>Morning</TopTitleAccent> begins <br /> with Coffee
+          </TopTitleSpan>
+          <TopLocation>
+            <svg
+              fill='currentColor'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                fillRule='evenodd'
+                d='M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z'
+                clipRule='evenodd'
+              ></path>
+            </svg>
+            <span>22 cafes near you</span>
+          </TopLocation>
+        </TopTitle>
+        <TopCharacter>
+          <TopCharacterImg src='https://images.vexels.com/media/users/3/294734/isolated/lists/1023420d295545fd3f51cdfb82dc5b18-self-esteem-coffee-character-cute-icon.png' />
+        </TopCharacter>
+        <MainCafeBox>
+          <MainCafeImg src='https://image.shutterstock.com/image-photo/butter-scone-strawberry-cream-600w-1551568154.jpg' />
+          <MainCafeTitleBox>
+            <MainCafeTitle></MainCafeTitle>
+            <MainCafeLocation></MainCafeLocation>
+          </MainCafeTitleBox>
+        </MainCafeBox>
+      </Top>
+      {loading
+        ? 'loading...'
+        : data?.seeCafes?.map((cafe, i) => <CafeBox {...cafe} key={i} />)}
+      <AddBtn onClick={() => navigation(routes.add)}>
+        <svg
+          fill='currentColor'
+          viewBox='0 0 20 20'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            fillRule='evenodd'
+            d='M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z'
+            clipRule='evenodd'
+          />
+        </svg>
+      </AddBtn>
+    </Layout>
+  );
 }
+const Top = styled.div`
+  width: 100%;
+  height: 40%;
+  padding: 0 30px;
+  padding-bottom: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.05);
+  position: relative;
+`;
+const TopTitle = styled.div`
+  width: 60%;
+  font-size: 33px;
+  font-weight: 600;
+`;
+const TopTitleSpan = styled.span`
+  color: ${(props) => props.theme.fontColor};
+`;
+const TopTitleAccent = styled.span`
+  color: #ffc426;
+`;
+
+const TopLocation = styled.div`
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+  opacity: 0.7;
+  svg {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+  }
+  span {
+    font-size: 12px;
+    color: ${(props) => props.theme.fontColor};
+  }
+`;
+const TopCharacter = styled.div`
+  width: 40%;
+`;
+const TopCharacterImg = styled.img`
+  width: 160px;
+`;
+
+const MainCafeBox = styled.div`
+  position: absolute;
+  bottom: -50px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  width: 80%;
+  height: 95px;
+  border-radius: 35px;
+  background-color: ${(props) => props.theme.bgColor};
+  box-shadow: ${(props) => props.theme.boxShadow};
+  display: flex;
+  align-items: center;
+`;
+const MainCafeImg = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  box-shadow: ${(props) => props.theme.boxShadow};
+`;
+const MainCafeTitleBox = styled.div``;
+const MainCafeTitle = styled.span``;
+const MainCafeLocation = styled.span``;
+
+const AddBtn = styled.button`
+  background-color: ${(props) => props.theme.pointColor};
+  border-radius: 50%;
+  position: absolute;
+  right: 10px;
+  bottom: 60px;
+  width: 50px;
+  height: 50px;
+  box-shadow: ${(props) => props.theme.boxShadow};
+  svg {
+    color: white;
+  }
+`;
